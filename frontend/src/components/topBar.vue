43d91a6c0a55f9f937ab668200cf474a9f8fdf26
Erica-WX <2544194336@qq.com>
2019-02-02 22:54:35 +0800

    初始化项目

---
+++ frontend/src/components/topBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-row>
+    <el-col :span="4" >
+      <div class="logo">
+        <div class="title"><i>Take-Out</i></div>
+      </div>
+    </el-col>
+    <el-col :span="20">
+      <div>
+        <el-menu :default-active="activeIndex"
+                 class="el-menu-demo"
+                 mode="horizontal"
+                 @select="handleSelect"
+                 background-color="dodgerblue"
+                 text-color="#fff"
+                 active-text-color="#000000"
+
+        >
+          <el-menu-item index="1">外卖</el-menu-item>
+          <el-menu-item index="2">登录</el-menu-item>
+          <el-menu-item index="3">注册</el-menu-item>
+          <el-menu-item index="4">注册</el-menu-item>
+
+        </el-menu>
+      </div>
+    </el-col>
+  </el-row>
+
+</template>
+
+<script>
+    export default {
+      name: "top-bar",
+      data() {
+        return {
+          activeIndex: '1',
+        }
+      },
+      methods: {
+        handleSelect(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      },
+    }
+</script>
+
+<style scoped>
+
+  .logo{
+    background-color: dodgerblue;
+    text-align: center;
+    /*font-size: 18px;*/
+    height: 60px;
+    line-height: 60px;
+    margin: auto;
+  }
+
+  .title{
+    font-size: 30px;
+  }
+
+  .title:hover{
+    cursor: pointer;
+  }
+
+  .c{
+    color: dodgerblue;
+  }
+
+</style>
